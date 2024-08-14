7fa2e64eb2caa24b472c0e6b7fd2e3d3328ba2fd
pcybriwsky <pcybriwsky@gmail.com>
2024-08-14 10:21:41 -0400

    Adding analytics tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Sidebar from './Sidebar.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 const Layout = ({ children }) => {
   return (
+    
     <div className="sm:flex">
       <Sidebar />
       <div className="flex-1 p-4 bg-gray-100 sm:pl-72">
         {children}
+        <Analytics />
       </div>
     </div>
   );
